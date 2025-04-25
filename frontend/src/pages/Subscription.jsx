9a6fe41837b82d6cbf77305ea9c4aa7ee2733491
greencheff <greencheff55@gmail.com>
2025-04-25 13:45:28 +0300

    Create Subscription.jsx

---
+++ frontend/src/pages/Subscription.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Subscription = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Abonelik Sayfası</h1>
+      <p>Burada abonelik planları ve ödeme bilgileri gösterilecek.</p>
+    </div>
+  );
+};
+
+export default Subscription;
