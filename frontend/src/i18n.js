fa904aad694d4611feb743ff314e90fc5963bcc7
greencheff <greencheff55@gmail.com>
2025-04-25 20:48:21 +0300

    Update i18n.js

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -2,13 +2,18 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 i18n.use(initReactI18next).init({
-  lng: "tr",
+  lng: "tr", // Varsayılan dil
   fallbackLng: "tr",
+  interpolation: {
+    escapeValue: false, // React zaten XSS'e karşı güvenlidir
+  },
   resources: {
     tr: {
       translation: {
         welcome: "Hoş geldiniz",
-        // Diğer çeviriler buraya
+        features: "Özellikler",
+        pricing: "Paketleri Gör",
+        // Buraya bileşenlerde kullanmak istediğin çevirileri ekleyebilirsin
       },
     },
   },
