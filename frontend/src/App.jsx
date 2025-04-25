7c7ad13a270e407de920f7265950b02db3298ecc
greencheff <greencheff55@gmail.com>
2025-04-25 21:23:15 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-// src/App.jsx
+// frontend/src/App.jsx
+import React from 'react'
+import Router from './router'
+import { AuthProvider } from './context/AuthContext'
 
-import React from 'react';
-import Router from './router'; // ya da './router.jsx'
-
-function App() {
-  return <Router />;
+export default function App() {
+  return (
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
+  )
 }
-
-export default App;
