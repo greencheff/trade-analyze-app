35d5f9a1537c966ee178465c3567b463c34061e4
greencheff <greencheff55@gmail.com>
2025-04-25 13:06:34 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
-import { AuthProvider } from './context/AuthContext.jsx';
-import { ThemeProvider } from './context/ThemeContext.jsx';
+
+import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
