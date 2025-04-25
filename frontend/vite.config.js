141bc10bfd1682c814977353aa0070253b7813c5
greencheff <greencheff55@gmail.com>
2025-04-25 13:58:02 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': 'https://trade-analyze-backend.onrender.com',
-    },
+    host: true, // veya '0.0.0.0'
+    port: 3000,
   },
 });
