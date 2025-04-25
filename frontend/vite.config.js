adc732695eacc7cb95fb45759588667a4d5485eb
greencheff <greencheff55@gmail.com>
2025-04-25 20:36:22 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // ana klasör
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   },
-  server: {
-    mimeTypes: {
-      '.css': 'text/css',
-    },
-  },
-})
+});
