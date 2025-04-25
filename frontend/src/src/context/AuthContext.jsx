6074d17c257a055fb25017ff079b7cfb943ada73
greencheff <greencheff55@gmail.com>
2025-04-25 13:02:50 +0300

    Add AuthContext.jsx

---
+++ frontend/src/src/context/AuthContext.jsx
@@ -0,0 +1,25 @@
+// src/context/AuthContext.jsx
+
+import { createContext, useContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (data) => {
+    setUser(data); // Örnek: { id: 1, name: 'Ali' }
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
