ee3ee1b5d94777b02f720ce2ff064196b026b6b4
greencheff <greencheff55@gmail.com>
2025-04-25 14:03:20 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // veya '0.0.0.0'
-    port: 3000,
+    host: true, // Sunucuyu tüm ağ üzerinden erişilebilir yapmak için
+    port: 10000, // Opsiyonel, istediğiniz portu belirleyebilirsiniz
+    allowedHosts: ['trade-analyze-front.onrender.com'], // Belirli hostlardan gelen talepleri kabul et
   },
 });
