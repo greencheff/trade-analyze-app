a2d5e61c95e514422d9afba74d86be5576c1e71e
greencheff <greencheff55@gmail.com>
2025-04-29 00:35:52 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// trade-analyze-frontend/vite.config.js
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  root: '.', // ana klasör
-  build: {
-    outDir: 'dist',
-  },
-});
+export default ({ mode }) => {
+  // .env dosyasını yükle
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [react()],
+    define: {
+      // tüm kodda import.meta.env.VITE_API_URL kullanılabilir
+      'process.env': env
+    }
+  })
+}
