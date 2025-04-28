9d16f814eb424c9ef9add1f117b4c2ae12456578
greencheff <greencheff55@gmail.com>
2025-04-28 22:58:21 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,8 +5,9 @@ from app.routes import analyze
 
 app = FastAPI()
 
+# Ana route olarak "/api" altında analyze route'unu dahil ediyoruz
 app.include_router(analyze.router, prefix="/api")
 
 @app.get("/")
 def read_root():
-    return {"message": "Trade Analyze Backend Çalışıyor."}
+    return {"message": "Trade Analyze Backend API Çalışıyor."}
